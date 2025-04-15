575dd91910170feec5516cf3f416a2570f8f65af
ShachiruRashmika2 <it22602046@my.sliit.lk>
2025-04-16 03:14:38 +0530

    add map

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import WorldMap from './components/worldMap'
+
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>bat man</div>
+    <WorldMap />
   
     </>
   )
