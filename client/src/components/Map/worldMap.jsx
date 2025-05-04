9699acba28dadfbb61bec6d3fab84a0b9b767d2b
ShachiruRashmika2 <it22602046@my.sliit.lk>
2025-05-05 03:05:33 +0530

    resolve path error

--- client/src/components/Map/worldMap.jsx
+++ client/src/components/Map/worldMap.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "../../styles/Map.css";
+import "../../styles/map.css";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
